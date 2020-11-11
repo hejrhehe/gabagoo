46f7d5dec38d7eb077935e22a3a6c2c8fe494eb7
hejrhehe <50504114+hejrhehe@users.noreply.github.com>
2020-11-11 17:09:59 -0500

    Add files via upload

---
+++ Games/ProjectPokemonCopies.lua
@@ -0,0 +1,18 @@
+
+
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "HallowHub | MaterialLua";
+	Text = "Credits: MaterialLua UI Library by Twink Marie! Go to: materiallua.ml for more information.";
+	Button1 = "Okay";
+	Duration = 5;
+})
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "HallowHub";
+	Text = "Credits: Scripting of this GUI by Hallowdynes.";
+	Button1 = "Okay";
+	Duration = 5;
+})
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hejrhehe/Hallowdynamic/main/PokeMesserUpper.txt"))()
